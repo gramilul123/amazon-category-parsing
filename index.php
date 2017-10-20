e1aee8b7716782a7621c5212e5ea0cdf87166829
Ramil Gilaev <rgilaev@simtechdev.com>
2017-10-20 18:24:21 +0300

    test

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-require_once '/AmazonConnect/AmazonECS.php';
+require_once 'AmazonConnect/AmazonECS.php';
 
 $params = $_GET;
-$params = array(
-    'marketplace' => 'de',
-    'tag' => 'test02dca-20',
-    'access_key_id' => 'AKIAJBEEAKRZCLDJQDVA',
-    'secret_access_key' => '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',
-    'browse_node' => '567135031'
-);
 print_r($params);
-/*$result = array();
+$result = array();
 
 if (empty($params['marketplace']) && empty($params['tag']) && empty($params['access_key_id']) && empty($params['secret_access_key']) && empty($params['browse_node'])) {
     $result = array();
@@ -23,5 +16,5 @@ try {
 } catch (\Exception $e) {
     $result = array('error' => $e->getMessage());
 }
-print_r($result);*/
+print_r($result);
 ?>
